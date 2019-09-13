e88719892d16af2c4eb1eedb89b7edefb32db5ef
aestheticsdata <aesthetics.data@gmail.com>
2019-09-13 12:15:55 +0200

    404 - done

--- pfa-client/src/components/notFoundComponent/StyledNotFoundComponent.js
+++ pfa-client/src/components/notFoundComponent/StyledNotFoundComponent.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import bank from './sharon-mccutcheon-8lnbXtxFGZw-unsplash-low-blured.jpg';
+import 'typeface-exo';
 
 const StyledNotFoundComponent = styled.div`
   position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   left: 0;
   right: 0;
   top: 60px;
@@ -16,22 +20,24 @@ const StyledNotFoundComponent = styled.div`
     background-size: cover;
   }
   .notfound404 {
+    font-family: 'Exo', sans-serif;
     display: block;
     position: absolute;
+    padding-top: 120px;
     z-index: 2;
     font-size: 120px;
     color: #fff;
-    background-color: rgba(100, 100, 100, 0.7);
+    background-color: rgba(100, 100, 100, 0.8);
     border-radius: 5px;
-    width: 400px;
-    height: 300px;
+    width: 500px;
+    height: 400px;
     text-align: center;
     top: 120px;
     
     .title {
     }
     .text {
-      font-size: 18px;
+      font-size: 24px;
     }
   }
 `;
