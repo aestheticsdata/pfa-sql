2d6e7e6ee2d0ff6ef01a9e7802a2c2c02767c9df
aestheticsdata <aesthetics.data@gmail.com>
2019-09-17 11:41:18 +0200

    daypicker select entire week

--- pfa-client/src/components/datePickerWrapper/DatePickerWrapper.js
+++ pfa-client/src/components/datePickerWrapper/DatePickerWrapper.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import onClickOutside from "react-onclickoutside";
+import moment from 'moment';
 
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
@@ -7,11 +8,36 @@ import 'react-day-picker/lib/style.css';
 import StyledDatePickerWrapper from './StyledDatePickerWrapper';
 
 
+function getWeekDays(weekStart) {
+  const days = [weekStart];
+  for (let i = 1; i < 7; i += 1) {
+    days.push(
+      moment(weekStart)
+        .add(i, 'days')
+        .toDate()
+    );
+  }
+  return days;
+}
+
+function getWeekRange(date) {
+  return {
+    from: moment(date)
+      .startOf('week')
+      .toDate(),
+    to: moment(date)
+      .endOf('week')
+      .toDate(),
+  };
+}
+
 class DatePickerWrapper extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isCalendarVisible: false,
+      hoverRange: null,
+      selectedDays: [],
     };
   }
 
@@ -27,19 +53,76 @@ class DatePickerWrapper extends Component {
     })
   };
 
+  handleDayChange = date => {
+    this.setState({
+      selectedDays: getWeekDays(getWeekRange(date).from),
+    });
+  };
+
+  handleDayEnter = date => {
+    this.setState({
+      hoverRange: getWeekRange(date),
+    });
+  };
+
+  handleDayLeave = () => {
+    this.setState({
+      hoverRange: undefined,
+    });
+  };
+
+  handleWeekClick = (weekNumber, days, e) => {
+    this.setState({
+      selectedDays: days,
+    });
+  };
+
   render() {
+    const { hoverRange, selectedDays } = this.state;
+
+    const daysAreSelected = selectedDays.length > 0;
+
+    const modifiers = {
+      hoverRange,
+      selectedRange: daysAreSelected && {
+        from: selectedDays[0],
+        to: selectedDays[6],
+      },
+      hoverRangeStart: hoverRange && hoverRange.from,
+      hoverRangeEnd: hoverRange && hoverRange.to,
+      selectedRangeStart: daysAreSelected && selectedDays[0],
+      selectedRangeEnd: daysAreSelected && selectedDays[6],
+    };
+
     return (
       <StyledDatePickerWrapper>
         <div
           className="caption"
           onClick={this.toggleCalendar}
         >
-          dates
+          {
+            selectedDays.length === 7 ?
+              <div>
+                {moment(selectedDays[0]).format('LL')} –{' '}
+                {moment(selectedDays[6]).format('LL')}
+              </div>
+              :
+              <div>dates</div>
+          }
         </div>
         <div className="date-picker">
           {
             this.state.isCalendarVisible ?
-              <DayPicker />
+              <DayPicker
+                selectedDays={selectedDays}
+                showWeekNumbers
+                showOutsideDays
+                modifiers={modifiers}
+                onDayClick={this.handleDayChange}
+                onDayMouseEnter={this.handleDayEnter}
+                onDayMouseLeave={this.handleDayLeave}
+                onWeekClick={this.handleWeekClick}
+              />
               :
               null
           }
--- pfa-client/src/components/datePickerWrapper/StyledDatePickerWrapper.js
+++ pfa-client/src/components/datePickerWrapper/StyledDatePickerWrapper.js
@@ -6,7 +6,49 @@ const StyledDatePickerWrapper = styled.div`
   }
   .date-picker {
     background-color: #2b2a2a;
+    
+    .DayPicker-Month {
+      border-collapse: separate;
+    }
+    .DayPicker-WeekNumber {
+      outline: none;
+    }
+    .DayPicker-Day {
+      outline: none;
+      border: 1px solid transparent;
+    }
+    .DayPicker-Day--hoverRange {
+      background-color: #60605c !important;
+    }
+  
+    .DayPicker-Day--selectedRange {
+      background-color: #fff7ba !important;
+      border-top-color: #FFEB3B;
+      border-bottom-color: #FFEB3B;
+      border-left-color: #fff7ba;
+      border-right-color: #fff7ba;
+    }
+  
+    .DayPicker-Day--selectedRangeStart {
+      background-color: #FFEB3B !important;
+      border-left: 1px solid #FFEB3B;
+    }
+  
+    .DayPicker-Day--selectedRangeEnd {
+      background-color: #FFEB3B !important;
+      border-right: 1px solid #FFEB3B;
+    }
+  
+    .DayPicker-Day--selectedRange:not(.DayPicker-Day--outside).DayPicker-Day--selected,
+    .DayPicker-Day--hoverRange:not(.DayPicker-Day--outside).DayPicker-Day--selected {
+      border-radius: 0 !important;
+      color: black !important;
+    }
+    .DayPicker-Day--hoverRange:hover {
+      border-radius: 0 !important;
+    }
   }
+  
 `;
 
 export default StyledDatePickerWrapper;
