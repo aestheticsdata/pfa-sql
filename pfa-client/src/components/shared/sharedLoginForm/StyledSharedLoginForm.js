13e662ff3b919586d4e9dcf250f904e21badfc4b
aestheticsdata <aesthetics.data@gmail.com>
2019-09-21 16:29:10 +0200

    forms mixins

--- pfa-client/src/components/shared/sharedLoginForm/StyledSharedLoginForm.js
+++ pfa-client/src/components/shared/sharedLoginForm/StyledSharedLoginForm.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import colors from '../../../colors';
+import {
+  buttonMixin,
+  inputMixin,
+} from '../sharedCSS/sharedFormsCSS';
 
 const StyledSharedLoginForm = styled.div`
   .container {
@@ -13,57 +17,17 @@ const StyledSharedLoginForm = styled.div`
     }
     
     input {
-      // hack to remove in Chrome, input colors
-      // https://webagility.com/posts/the-ultimate-list-of-hacks-for-chromes-forced-yellow-background-on-autocompleted-inputs
-      // https://stackoverflow.com/questions/34551637/css-webkit-transition-not-working-on-input-type
-      // https://github.com/styled-components/styled-components/issues/492
-      &:-webkit-autofill,
-      &:-webkit-autofill:hover,
-      &:-webkit-autofill:focus,
-      &:-webkit-autofill:active {
-        -webkit-transition-delay: 9999s;
-      }
-      // ////////////////////////////////////////
-      color: inherit;
-      font-size: 15px;
-      background-color: transparent;
-      border-bottom: 1px solid ${colors.formsGlobalColor};
-      margin: 8px 0;
-      outline: none;
-      width: 100%;
-      padding-bottom: 10px;
-      &:focus {
-        outline: none;
-        border-bottom: 2px solid ${colors.formsGlobalColor};
-        transition: all .2s ease;
-      }
+      ${inputMixin};
     }
     
     .shared-login-submit-btn {
-      color: inherit;
-      text-transform: uppercase;
+      ${buttonMixin};
+      
       margin: 40px 0 20px 0;
       font-size: 18px;
-      background: transparent;
-      border: 1px solid ${colors.formsGlobalColor};
       width: 100%;
       height: 30px;
-      border-radius: 5px;
-      cursor: pointer;
-      outline: none;
       
-      &:disabled {
-        opacity: 0.5;
-        cursor: not-allowed;
-      }
-      
-      &:hover:enabled {
-        box-shadow: 0 1px 10px 1px rgba(150, 150, 150, 0.6);
-        background-color: rgba(200, 200, 200, 0.1);
-        color: ${colors.formsGlobalColorHover};
-        border: 1px solid ${colors.formsGlobalColorHover};
-        transition: all .2s ease;
-      }
     }
   }
 `;
--- pfa-client/src/components/spendings/spendingDayItem/spendingModal/StyledSpendingModal.js
+++ pfa-client/src/components/spendings/spendingDayItem/spendingModal/StyledSpendingModal.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components/macro';
 import colors from '../../../../colors';
+import {
+  buttonMixin,
+  inputMixin,
+} from '../../../shared/sharedCSS/sharedFormsCSS';
 
 const StyledSpendingModal = styled.div`
   background-color: ${colors.spendingItemHover};
@@ -14,57 +18,16 @@ const StyledSpendingModal = styled.div`
   padding: 0 10px;
   
   input {
-    // hack to remove in Chrome, input colors
-    // https://webagility.com/posts/the-ultimate-list-of-hacks-for-chromes-forced-yellow-background-on-autocompleted-inputs
-    // https://stackoverflow.com/questions/34551637/css-webkit-transition-not-working-on-input-type
-    // https://github.com/styled-components/styled-components/issues/492
-    &:-webkit-autofill,
-    &:-webkit-autofill:hover,
-    &:-webkit-autofill:focus,
-    &:-webkit-autofill:active {
-      -webkit-transition-delay: 9999s;
-    }
-    // ////////////////////////////////////////
-    color: ${colors.blueNavy};
-    font-size: 15px;
-    background-color: transparent;
-    border-bottom: 1px solid ${colors.formsGlobalColor};
-    margin: 8px 0;
-    outline: none;
-    width: 100%;
-    padding-bottom: 10px;
-    &:focus {
-      outline: none;
-      border-bottom: 2px solid ${colors.formsGlobalColor};
-      transition: all .2s ease;
-    }
+    ${inputMixin};
   }
   
   .spending-btn {
-    color: inherit;
+    ${buttonMixin};
+    
     display: block;
-    text-transform: uppercase;
     margin: 10px auto;
-    background: transparent;
-    border: 1px solid ${colors.formsGlobalColor};
     width: 50%;
     height: 20px;
-    border-radius: 5px;
-    cursor: pointer;
-    outline: none;
-    
-    &:disabled {
-      opacity: 0.5;
-      cursor: not-allowed;
-    }
-    
-    &:hover:enabled {
-      box-shadow: 0 1px 10px 1px rgba(150, 150, 150, 0.6);
-      background-color: rgba(200, 200, 200, 0.1);
-      color: ${colors.formsGlobalColorHover};
-      border: 1px solid ${colors.formsGlobalColorHover};
-      transition: all .2s ease;
-    }
   }
 `;
 
