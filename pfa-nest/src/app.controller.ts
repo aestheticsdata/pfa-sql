8246817b8e833a1f3010313edb1b9a4875eb46be
aestheticsdata <aesthetics.data@gmail.com>
2020-10-08 18:22:30 +0200

    adding config and mongoose connection

--- pfa-nest/src/app.controller.ts
+++ pfa-nest/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly configService: ConfigService) {}
 
   @Get()
   getHello(): string {
+    console.log('this.configService', this.configService);
+    console.log(this.configService.get<string>('PFA_EMAIL_USER'));
+    console.log('process.env.PFA_EMAIL_USER', process.env.PFA_EMAIL_USER);
     return this.appService.getHello();
   }
 }
--- pfa-nest/src/app.module.ts
+++ pfa-nest/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // this is needed if `ConfigModule.forRoot({ isGlobal: true })` is not set
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('ATLAS_URI'),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
