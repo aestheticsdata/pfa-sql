e4ff3620be8c7594641a351e6a8b58177e214149
aestheticsdata <aesthetics.data@gmail.com>
2021-02-24 11:26:20 +0100

    Dropdown class -> functional component

--- pfa-client/src/components/common/dropdown/Dropdown.js
+++ pfa-client/src/components/common/dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown} from '@fortawesome/free-solid-svg-icons';
 import useOnClickOutside from 'use-onclickoutside';
@@ -6,74 +6,50 @@ import useOnClickOutside from 'use-onclickoutside';
 import StyledDropDown from './StyledDropDown';
 
 
-// const DropDown = () => {
-//
-// }
+const DropDown = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
 
-class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-  }
+  const toggleDropdown = () => { setIsOpen(!isOpen) };
 
-  toggleDropdown = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    })
-  };
+  const handleClickOutside = () => { setIsOpen(false) };
+  useOnClickOutside(ref, handleClickOutside);
 
-  handleClickOutside = ev => {
-    this.setState({
-      isOpen: false,
-    })
-  };
+  const close = () => { setIsOpen(false) };
 
-  close = ev => {
-    this.setState({
-      isOpen: false,
-    })
-  };
-
-  render() {
-    const { isOpen } = this.state;
-
-    return (
-      <StyledDropDown>
-        <div className="container">
-          <div
-            onClick={this.toggleDropdown}
-          >
-            <>
-              {
-                isOpen ?
-                  <FontAwesomeIcon
-                    className="icon"
-                    icon={faAngleUp}
-                  />
-                  :
-                  <FontAwesomeIcon
-                    className="icon"
-                    icon={faAngleDown}
-                  />
-              }
-            </>
-            {this.props.children[0]}
-          </div>
-          <div>
+  return (
+    <StyledDropDown ref={ref}>
+      <div className="container">
+        <div
+          onClick={toggleDropdown}
+        >
+          <>
             {
               isOpen ?
-                React.cloneElement(this.props.children[1], { handleclosefromchild: () => this.close()})
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={faAngleUp}
+                />
                 :
-                null
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={faAngleDown}
+                />
             }
-          </div>
+          </>
+          { children[0] }
         </div>
-      </StyledDropDown>
-    )
-  }
+        <div>
+          {
+            isOpen ?
+              React.cloneElement(children[1], { handleclosefromchild: () => close()})
+              :
+              null
+          }
+        </div>
+      </div>
+    </StyledDropDown>
+  )
 }
 
-// export default onClickOutside(Dropdown);
-export default Dropdown;
+export default DropDown;
