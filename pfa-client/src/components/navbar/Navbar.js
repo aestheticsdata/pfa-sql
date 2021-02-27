19c1315fe5f678d17be2330ae37d41bc91ec922d
aestheticsdata <aesthetics.data@gmail.com>
2021-02-27 21:48:27 +0100

    NavBar class to functional

--- pfa-client/src/components/navbar/Navbar.js
+++ pfa-client/src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import getSymbolFromCurrency from 'currency-symbol-map';
 
@@ -14,71 +14,63 @@ import messages from './messages';
 import UserMenu from './userMenu/UserMenu';
 import LangMenu from './langMenu/LangMenu';
 
-class NavBar extends Component {
-  render() {
-    const { token } = this.props;
-    const user = JSON.parse(localStorage.getItem('pfa-user'));
 
-    return (
-      <StyledNavBar>
-        <div className="logo">
-          <Logo />
-        </div>
-        {
-          token ?
-            <>
-              <NavLink to='/' className="link" exact>
-                <FormattedMessage
-                  { ...messages.spendings }
-                />
-              </NavLink>
-              <NavLink to='/stats' className="link" exact>
-                <FormattedMessage
-                  { ...messages.stats }
-                />
-              </NavLink>
-              <div className="date-picker-wrapper">
-                <DatePickerWrapper />
-              </div>
-              <UserMenu
-                className="usermenu"
-                user={user}
-              />
-              <LangMenu
-                className="langmenu"
-              />
-              <div className="base-currency">
-                <FormattedMessage
-                  { ...messages.globalCurrency }
-                /> :
-                <span className="symbol">{ getSymbolFromCurrency(user.baseCurrency) }</span>
-              </div>
-            </>
-            :
-            <>
-              <NavLink to='/login' className="link" exact>
-                <FormattedMessage
-                  { ...messages.login }
-                />
-              </NavLink>
-              <NavLink to='/register' className="link" exact>
-                <FormattedMessage
-                  { ...messages.signUp }
-                />
-              </NavLink>
-            </>
+const NavBar = ( ) => {
+  const user = JSON.parse(localStorage.getItem('pfa-user'));
+  const token = useSelector(state => state.loginReducer.token);
 
-        }
-      </StyledNavBar>
-    );
-  }
+  return (
+    <StyledNavBar>
+      <div className="logo">
+        <Logo />
+      </div>
+      {
+        token ?
+          <>
+            <NavLink to='/' className="link" exact>
+              <FormattedMessage
+                { ...messages.spendings }
+              />
+            </NavLink>
+            <NavLink to='/stats' className="link" exact>
+              <FormattedMessage
+                { ...messages.stats }
+              />
+            </NavLink>
+            <div className="date-picker-wrapper">
+              <DatePickerWrapper />
+            </div>
+            <UserMenu
+              className="usermenu"
+              user={user}
+            />
+            <LangMenu
+              className="langmenu"
+            />
+            <div className="base-currency">
+              <FormattedMessage
+                { ...messages.globalCurrency }
+              /> :
+              <span className="symbol">{ getSymbolFromCurrency(user.baseCurrency) }</span>
+            </div>
+          </>
+          :
+          <>
+            <NavLink to='/login' className="link" exact>
+              <FormattedMessage
+                { ...messages.login }
+              />
+            </NavLink>
+            <NavLink to='/register' className="link" exact>
+              <FormattedMessage
+                { ...messages.signUp }
+              />
+            </NavLink>
+          </>
+
+      }
+    </StyledNavBar>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    token: state.loginReducer.token,
-    user: state.loginReducer.user,
-  }
-};
-
-export default connect(mapStateToProps, null)(NavBar);
+export default NavBar;
