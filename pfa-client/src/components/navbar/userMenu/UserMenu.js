49b52315d8a75a367f5b5df7d5c452e0e8799738
aestheticsdata <aesthetics.data@gmail.com>
2021-02-27 21:59:25 +0100

    UserMenu class to functional

--- pfa-client/src/components/navbar/userMenu/UserMenu.js
+++ pfa-client/src/components/navbar/userMenu/UserMenu.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { injectIntl } from 'react-intl';
-import { connect } from 'react-redux';
-// import Swal from 'sweetalert2';
+import { useIntl } from 'react-intl';
+import { useSelector } from 'react-redux';
 
 import {
   faSignOutAlt,
   faKey,
-  // faCoins,
 } from '@fortawesome/free-solid-svg-icons';
 
 import Dropdown from '../../common/dropdown/Dropdown';
@@ -14,40 +12,23 @@ import UserMenuContent from './UserMenuContent';
 import StyledUserMenu from './StyledUserMenu';
 import { history } from '../../../history';
 import messages from './messages';
-// import { changeBaseCurrency } from './actions';
-// import { default as currencyCodes } from  '../../../currency-codes';
 
-const UserMenu = (props) => {
-  // const currencies = Object.assign({}, ...currencyCodes.map(currency => {
-  //   return { [currency.code]: currency.name }
-  // }));
-  // const openModalCurrency = () => {
-  //   Swal.fire({
-  //     title: 'change currency',
-  //     input: 'select',
-  //     inputOptions: currencies,
-  //     inputValue: props.user.baseCurrency,
-  //     inputPlaceholder: 'Select a currency',
-  //     showCancelButton: true,
-  //   }).then((currencyObject) => currencyObject.value && props.changeBaseCurrency(props.user.id, currencyObject.value));
-  // };
+
+const UserMenu = () => {
+  const intl = useIntl();
+
+  const user = useSelector(state => state.loginReducer.user);
 
   const listItems = [
     {
       id: 'changepassword',
-      label: props.intl.formatMessage({ ...messages.changePassword }),
+      label: intl.formatMessage({ ...messages.changePassword }),
       icon: faKey,
       callback: () => history.push('/changepassword'),
     },
-    // {
-    //   id: 'changebasecurrency',
-    //   label: props.intl.formatMessage({ ...messages.changeBaseCurrency }),
-    //   icon: faCoins,
-    //   callback: () => openModalCurrency(),
-    // },
     {
       id: 'logout',
-      label: props.intl.formatMessage({ ...messages.logout }),
+      label: intl.formatMessage({ ...messages.logout }),
       icon: faSignOutAlt,
       callback: () => history.push('/logout'),
     },
@@ -56,7 +37,7 @@ const UserMenu = (props) => {
   return (
     <StyledUserMenu>
       <Dropdown>
-        <span className="email">{props.user.email}</span>
+        <span className="email">{user.email}</span>
         <UserMenuContent
           listItems={listItems}
         />
@@ -65,18 +46,5 @@ const UserMenu = (props) => {
   )
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.loginReducer.user,
-  }
-};
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     changeBaseCurrency: (userID, currency) => dispatch(changeBaseCurrency(userID, currency)),
-//   }
-// };
-
-// export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(UserMenu));
-export default injectIntl(connect(mapStateToProps, null)(UserMenu));
+export default UserMenu;
 
