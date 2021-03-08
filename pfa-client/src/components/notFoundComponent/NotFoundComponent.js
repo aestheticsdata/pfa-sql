13ac3f0c77adb611e6418747411e603678deb791
aestheticsdata <aesthetics.data@gmail.com>
2021-03-08 22:58:37 +0100

    NotFound component intl hook

--- pfa-client/src/components/notFoundComponent/NotFoundComponent.js
+++ pfa-client/src/components/notFoundComponent/NotFoundComponent.js
@@ -1,17 +1,21 @@
-import { injectIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import StyledNotFoundComponent from './StyledNotFoundComponent';
 import messages from './messages';
 
-const NotFoundComponent = (props) => (
-  <StyledNotFoundComponent>
-    <div className="notfound404">
-      <div className="title">404</div>
-      <div className="text">{props.intl.formatMessage({ ...messages.text })}</div>
-    </div>
-    <div className="bank" />
-  </StyledNotFoundComponent>
-);
+const NotFoundComponent = () => {
+  const intl = useIntl();
 
-export default injectIntl(NotFoundComponent);
+  return (
+    <StyledNotFoundComponent>
+      <div className="notfound404">
+        <div className="title">404</div>
+        <div className="text">{intl.formatMessage({ ...messages.text })}</div>
+      </div>
+      <div className="bank" />
+    </StyledNotFoundComponent>
+  )
+};
+
+export default NotFoundComponent;
 
