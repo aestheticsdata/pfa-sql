76b20ecb8c5373d95aac03739eb90124083f0036
aestheticsdata <aesthetics.data@gmail.com>
2021-03-08 22:29:09 +0100

    Register components class to functional

--- pfa-client/src/components/register/Register.js
+++ pfa-client/src/components/register/Register.js
@@ -1,78 +1,65 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
-import { injectIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 
 import {
   register,
   clearRegisterFailed,
 } from './actions';
 
-import SharedLoginForm from "../shared/sharedLoginForm/SharedLoginForm";
 import StyledRegister from './StyledRegister';
-import StyledSharedLoginContainer from '../shared/sharedLoginContainer/StyledSharedLoginContainer';
+import SharedLoginForm from "@components/shared/sharedLoginForm/SharedLoginForm";
+import StyledSharedLoginContainer from '@components/shared/sharedLoginContainer/StyledSharedLoginContainer';
 import messages from './messages';
 
-class Register extends Component {
-  onSubmit = (values, { setSubmitting }) => {
-    console.log('1 : ', values);
-    this.props.register(values.email, values.password, values.currency);
+
+const Register = ({ history }) => {
+  const dispatch = useDispatch();
+  const intl = useIntl();
+
+  const token = useSelector(state => state.loginReducer.token);
+  const registerFailed =  useSelector(state => state.registerReducer.failed);
+  const registerErrorMessage = useSelector(state => state.registerReducer.errorMessage);
+
+  const onSubmit = (values, { setSubmitting }) => {
+    dispatch(register(values.email, values.password, values.currency));
     setSubmitting(false);
   };
 
-  componentDidMount() {
+  useEffect(() => {
     if (localStorage.getItem('pfa-token')) {
-      this.props.history.push('/');
+      history.push('/');
     }
-  }
+  }, [token]);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.token !== prevProps.token) {
-      this.props.history.push('/');
-    }
-    if (this.props.registerFailed) {
+  useEffect(() => {
+    if (registerFailed) {
       Swal.fire({
         title: 'Error',
-        text: this.props.registerErrorMessage,
+        text: registerErrorMessage,
         type: 'error',
         confirmButtonText: 'close',
         onClose: () => {
-          this.props.clearRegisterFailed()
+          dispatch(clearRegisterFailed());
         }
       })
     }
-  }
+  }, [registerFailed]);
 
-  render() {
-    return (
-      <StyledRegister>
-        <StyledSharedLoginContainer>
-          <SharedLoginForm
-            onSubmit={this.onSubmit}
-            buttonTitle={this.props.intl.formatMessage({ ...messages.buttonLabel })}
-            displayEmailField
-            displayPasswordField
-            displayCurrencyField
-          />
-        </StyledSharedLoginContainer>
-      </StyledRegister>
-    );
-  }
-};
+  return (
+    <StyledRegister>
+      <StyledSharedLoginContainer>
+        <SharedLoginForm
+          onSubmit={onSubmit}
+          buttonTitle={intl.formatMessage({ ...messages.buttonLabel })}
+          displayEmailField
+          displayPasswordField
+          displayCurrencyField
+        />
+      </StyledSharedLoginContainer>
+    </StyledRegister>
+  );
+}
 
-const mapStateToProps = (state) => {
-  return {
-    token: state.loginReducer.token,
-    registerFailed: state.registerReducer.failed,
-    registerErrorMessage: state.registerReducer.errorMessage,
-  }
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    register: (email, password, currency) => dispatch(register(email, password, currency)),
-    clearRegisterFailed: () => dispatch(clearRegisterFailed()),
-  }
-};
-
-export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(Register));
+export default Register;
