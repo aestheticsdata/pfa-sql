52310fe98f95b4e80176818c7aabbea97f7e85b8
aestheticsdata <aesthetics.data@gmail.com>
2021-04-04 00:31:38 +0200

    user sign in controller promises to async

--- src/routes/controllers/users/signInController.js
+++ src/routes/controllers/users/signInController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const { User } = require('../../../db/dbInit');
 
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { email, password } = req.body;
 
   // Simple validation
@@ -12,16 +12,16 @@ module.exports = (req, res) => {
   }
 
   // Check for existing user
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (!user) return res.status(400).json({ message: 'User Does not exist' });
+  try {
+    const user = await User.findOne({ where: { email } })
+    if (!user) return res.status(400).json({ message: 'User Does not exist' });
 
-      // Validate password
-      bcrypt.compare(password, user.password)
-        .then(isMatch => {
-          if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
-          signIn(res, user);
-        })
-        .catch(err => res.status(400).json(`Error while getting user : ${err}`));
-    })
+    // Validate password
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+    if (!isMatchPassword) return res.status(400).json({ message: 'Invalid credentials' });
+    signIn(res, user);
+
+  } catch (err) {
+    res.status(400).json(`Error while getting user : ${err}`)
+  }
 };
