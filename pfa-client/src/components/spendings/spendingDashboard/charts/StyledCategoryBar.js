a12d423e078bf077697caefea215c53263638b5a
aestheticsdata <aesthetics.data@gmail.com>
2021-05-11 13:44:07 +0200

    weekly charts minor changes

--- pfa-client/src/components/spendings/spendingDashboard/charts/StyledCategoryBar.js
+++ pfa-client/src/components/spendings/spendingDashboard/charts/StyledCategoryBar.js
@@ -1,16 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const stripes = css`
+  background: repeating-linear-gradient(
+    135deg,
+    white,
+    white 10px,
+    #eee 10px,
+    #eee 11px
+  );
+`;
 
 const StyledCategoryBar = styled.div`
   background-color: ${props => props.bgcolor || '#fff'};
-  ${props => props.bgcolor === null && `
-    background: repeating-linear-gradient(
-      135deg,
-      white,
-      white 10px,
-      #eee 10px,
-      #eee 11px
-    );
-  `}
+  ${props => props.bgcolor === null && stripes}
   .bar {
     height: 100%;
     span {
--- src/routes/controllers/spendings/getSpendingsWeeklyChartsController.js
+++ src/routes/controllers/spendings/getSpendingsWeeklyChartsController.js
@@ -12,14 +12,15 @@ module.exports = async (req, res) => {
       SELECT amount as value, name as label, color as bgcolor
       FROM 
         (
-            SELECT categoryID, SUM(amount) as amount
-            FROM Spendings
-            LEFT JOIN Categories ON Spendings.categoryID = Categories.ID
-            WHERE Spendings.userID = '${req.query.userID}'
-            AND Spendings.date BETWEEN '${from}' AND '${to}'
-            GROUP BY categoryID
+          SELECT categoryID, SUM(amount) as amount
+          FROM Spendings
+          LEFT JOIN Categories ON Spendings.categoryID = Categories.ID
+          WHERE Spendings.userID = '${req.query.userID}'
+          AND Spendings.date BETWEEN '${from}' AND '${to}'
+          GROUP BY categoryID
         ) as TableTemp
-        LEFT JOIN Categories on TableTemp.categoryID = Categories.ID;
+        LEFT JOIN Categories on TableTemp.categoryID = Categories.ID
+        ORDER BY amount DESC;
     `);
     res.status(200).json(chartsStats);
   } catch (e) {
