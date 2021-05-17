cf0034e24de3b9d2572088b6a0ac4e9cfc9c8ec1
aestheticsdata <aesthetics.data@gmail.com>
2021-05-17 15:53:03 +0200

    spendings css - flex

--- pfa-client/src/components/spendings/StyledSpendings.js
+++ pfa-client/src/components/spendings/StyledSpendings.js
@@ -6,22 +6,27 @@ const StyledSpendings = styled.div`
   position: relative;
   top: ${cssSizes.navbarHeight + cssSizes.dashboardHeight}px;
   padding-top: 10px;
-  
-  .spendings-container {
+
+  .list-container {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    height: auto;
-    overflow: auto;
-    width: 1520px;
-    margin: 20px auto 10px auto;
-    
-    @media(max-width: 1524px) {
-      width: 1050px;
-    }
-    
-    @media(max-width: 1024px) {
-      width: 500px;
+    justify-content: center;
+  
+    .spendings-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      height: auto;
+      overflow: auto;
+      width: 1515px;
+      margin-top: 20px;
+      
+      @media(max-width: 1524px) {
+        width: 1010px;
+      }
+      
+      @media(max-width: 1024px) {
+        width: 500px;
+      }
     }
   }
 `;
