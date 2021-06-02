155e8ca20f5818f05b0f8f466e4efd26a80bf1e5
aestheticsdata <aesthetics.data@gmail.com>
2021-06-02 14:16:54 +0200

    cron

--- src/cron/cron-mysql.js
+++ src/cron/cron-mysql.js
@@ -1,13 +1,15 @@
 const CronJob = require('cron').CronJob;
 
+
 const mysqlDump = () => {
+  console.log('mysqlDump', new Date());
   const exec = require('child_process').exec;
-  exec(`mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} ${process.env.DB} > ${process.env.PFA_DUMP_PATH}`);
+  exec(`mysqldump -u ${process.env.DB_USER} -p${process.env.DB_PASSWORD} ${process.env.DB} > ${process.env.PFA_DUMP_PATH}pfadump.sql`);
 }
 
 module.exports = () => {
   new CronJob(
-    '* * * * * *',
+    '0 0 */2 * * *', // warning: first star is second, it extends the classic cron syntax
     mysqlDump,
     null,
     true,
