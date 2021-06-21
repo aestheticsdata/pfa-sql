6175405557f69058b35d179c54a2c18510c83b8c
aestheticsdata <aesthetics.data@gmail.com>
2021-06-21 16:07:20 +0200

    unit tests

---
+++ src/routes/controllers/categories/tests/updateCategoryController.spec.js
@@ -0,0 +1,57 @@
+const prisma = require('../../../../db/dbInit');
+const updateCategoryController = require('../updateCategoryController');
+
+jest.mock('prisma');
+
+describe('updateCategoryController', () => {
+  const req = {
+    body: {
+      ID: '123',
+      name: 'alimentation',
+      color: '#fff',
+      userID: '1',
+    }
+  }
+
+  const categories = [
+    {
+      ID: '123',
+      name: 'alimentation',
+      color: '#fff',
+      userID: '1',
+    },
+    {
+      ID: '444',
+      name: 'foo',
+      color: '#ccc',
+      userID: '1',
+    }
+  ];
+
+  const res = {
+    json: jest.fn(categories => { console.log('res: ', categories) }),
+    status: _code => ({ json: err => err }),
+  };
+
+  const categoryError = new Error('category update error');
+
+  it('should update the category and return some categories', async () => {
+    prisma.categories = {
+      update: () => true,
+      findMany: () => categories,
+    }
+    await updateCategoryController(req, res);
+
+    expect(res.json).toHaveBeenCalledWith(categories);
+  });
+
+  it('should return an error', async () => {
+    prisma.categories = {
+      update: () => {throw categoryError},
+      findMany: () => categories,
+    }
+    const result = await updateCategoryController(req, res);
+    expect(result).toEqual(categoryError);
+  });
+});
+
