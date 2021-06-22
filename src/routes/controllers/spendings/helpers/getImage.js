1de7935f4678a21b0b980ea944eaf7c00d4f82b1
aestheticsdata <aesthetics.data@gmail.com>
2021-06-22 20:18:10 +0200

    removing unnecessary code

--- src/routes/controllers/spendings/helpers/getImage.js
+++ src/routes/controllers/spendings/helpers/getImage.js
@@ -4,7 +4,7 @@ const { uploadPath } = require('./constants');
 
 module.exports = async (invoicefile, userID) => {
   const imageFile = await readFile(uploadPath + '/' + userID + '/' + invoicefile);
-  const base64Image = Buffer.from(imageFile).toString('base64');
+  const base64Image = imageFile.toString('base64');
   const contentType = `image/${invoicefile.split('.').pop()}`;
   const invoiceImageString = `data:${contentType};base64,${base64Image}`;
 
