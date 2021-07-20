57cbaab4ed0a16cabf167b5c1306fa5af7e6c8a6
aestheticsdata <aesthetics.data@gmail.com>
2021-07-20 16:24:36 +0200

    mysqldump empty file bug fix

--- src/cron/cron-mysql.js
+++ src/cron/cron-mysql.js
@@ -27,8 +27,7 @@ const mysqlDump = () => {
   console.log('mysqlDump', new Date());
   const exec = require('child_process').exec;
   exec(`
-    mysqldump -u
-    ${process.env.DB_USER} -p${process.env.DB_PASSWORD} ${process.env.DB} > ${process.env.PFA_DUMP_PATH}pfadump.sql
+    mysqldump -u${process.env.DB_USER} -p${process.env.DB_PASSWORD} ${process.env.DB} > ${process.env.PFA_DUMP_PATH}pfadump.sql
   `,
     backupDB,
   );
